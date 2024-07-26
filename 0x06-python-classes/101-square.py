ba49bb539f81b9f1dde0258fccf374ba150e1873
olaguru <dapoodedeyi03@gmail.com>
2024-07-26 19:43:13 +0100

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -61,7 +61,7 @@ class Square():
     def __str__(self):
         """string rep... of the square"""
         if self.__size != 0:
-        [print() for i in range(0, self.__position[1])]
+            [print() for i in range(0, self.__position[1])]
         for i in range(self.__size):
             [print(" ", end="") for item in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
